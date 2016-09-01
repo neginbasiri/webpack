dbeba28b53048fba7b74a0022fd0c321d7f492e6
Negin <negin.basiri@oakton.com.au>
2016-09-01 15:42:07 +1000

    using webpack 2, tree shaking and different env variable

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './js/App';
 
-require('../scss/style.scss');
+require('./scss/style.scss');
 
 ReactDOM.render(<App />, document.getElementById('app'));
